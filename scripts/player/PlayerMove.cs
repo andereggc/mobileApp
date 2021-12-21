1bb58ac77904d14315383192b4a3c8394f8f94ce
kolade2 <koladea@bu.edu>
2021-12-20 20:55:39 -0500

    first player movement

--- scripts/player/PlayerMove.cs
+++ scripts/player/PlayerMove.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//script class
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //variable for player speed
+    public float moveSpeed = 3;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //moves the player object forward relative to the game speed
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
     }
 }
